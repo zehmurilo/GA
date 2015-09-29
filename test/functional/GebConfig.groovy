9925b96a182df325390dd0fb35d07dbea7cb81bb
João Pedro de Medeiros Santos <jpms2@cin.ufpe.br>
2015-09-29 14:23:28 -0300

    Update GebConfig.groovy

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,13 +1,25 @@
+import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
 
-driver = { 
-    	//set the firefox locale to 'en-us' since the tests expect english
-    	//see http://stackoverflow.com/questions/9822717 for more details
-    	FirefoxProfile profile = new FirefoxProfile()
-    	profile.setPreference("intl.accept_languages", "en-us")
-	def driverInstance = new FirefoxDriver(profile) 
-	driverInstance.manage().window().maximize() 
-	driverInstance
+driver = {
+//def driver = new HtmlUnitDriver()
+//driver.javascriptEnabled = true
+//driver
+def driver = new FirefoxDriver()
+driver
+} 
+
+environments {
+   // run as “grails -Dgeb.env=chrome test-app”
+   // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+   chrome {
+     driver = { new ChromeDriver() }
+   }
+
+   // run as “grails -Dgeb.env=firefox test-app”
+   // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+   firefox {
+    driver = { new FirefoxDriver() }
+  }
 }
-baseUrl = "http://localhost:8080/"
