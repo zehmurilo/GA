54e64d30d6abf933749b99ff129a4034a119c191
João Pedro de Medeiros Santos <jpms2@cin.ufpe.br>
2015-12-16 18:19:09 -0300

    Update GebConfig.groovy

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -2,24 +2,24 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
 
-driver = {
-//def driver = new HtmlUnitDriver()
-//driver.javascriptEnabled = true
-//driver
-def driver = new FirefoxDriver()
-driver
-} 
+baseUrl = "http://localhost:8080/"
 
 environments {
-   // run as “grails -Dgeb.env=chrome test-app”
-   // See: http://code.google.com/p/selenium/wiki/ChromeDriver
-   chrome {
-     driver = { new ChromeDriver() }
-   }
+    // run as “grails -Dgeb.env=chrome test-app”
+    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+    chrome {
+        driver = { File file = new File("chromedrivers/chromedriver.exe"); //configurar com o enderço correto do chromedriver.
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+            new ChromeDriver(); }
+    }
 
-   // run as “grails -Dgeb.env=firefox test-app”
-   // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-   firefox {
-    driver = { new FirefoxDriver() }
-  }
+    // run as “grails -Dgeb.env=firefox test-app”
+    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+    firefox {
+        driver = {FirefoxProfile profile = new FirefoxProfile()
+            profile.setPreference("intl.accept_languages", "en-us")
+            def driverInstance = new FirefoxDriver(profile)
+            driverInstance.manage().window().maximize()
+            driverInstance}
+    }
 }
