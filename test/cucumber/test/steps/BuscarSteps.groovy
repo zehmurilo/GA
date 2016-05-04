bd2ce5f3440163c4294c8f6c0586e0a7d30adcf2
zehmurilo <zehm14@gmail.com>
2016-05-04 16:56:19 -0300

    Update BuscarSteps.groovy

--- test/cucumber/test/steps/BuscarSteps.groovy
+++ test/cucumber/test/steps/BuscarSteps.groovy
@@ -1,28 +1,30 @@
-import steps.TestDataAndOperations
+import steps.*
 import static cucumber.api.groovy.EN.*
 
 Given(~'^"([^"]*)" está cadastrado no sistema$') { 
-String name -> addAtleta(name, null, null, null)
-assert atletas.findByNome(name) != null
+  String name -> addAtleta(name, null, null, null)
+  assert atletas.findByNome(name) != null
 }
 
 When(~'^busco pelo atleta de nome "([^"]*)"$') { 
-String name -> assert atletas.findByNome(name) != null
+  String name -> assert atletas.findByNome(name) != null
 }
 
-Then(~'^É retornado datalhes do atleta procurado$') { 
-assert atletas.getInfo(atletas.findByNome(name)) != null
+Then(~'^É retornado datalhes do atleta "([^"]*)"$') { String name ->
+  assert atletas.getInfo(atletas.findByNome(name)) != null
 }
 
-Given(~'^estou no menu de atletas e não existe atleta com nome "([^"]*)"$') {
-String name -> jogador = TestDataAndOperations.findByNome(name)
-assert jogador == null
+Given(~'^estou no menu de atletas e existe atleta com nome "([^"]*)"$') {
+  String name -> addAtleta(name, null, null, null)
+  at menuAtletasPage
 }
 
-When(~'^é feita uma busca pelo nome "([^"]*)"$') {
-String name -> jogador = TestDataAndOperations.findByNome(name)
+When(~'^é feita uma busca pelo nome "([^"]*)"$') { String name ->
+  at menuAtletasPage
+  page.selectBuscarAtletaNome(name)
 }
 
-Then(~'^é avisado na tela que um erro ocorreu$') {
-flash.message = "Jogador não existente"
+Then(~'^Aparece na tela os dados do atleta "([^"]*)"$') { String name->
+  at menuAtletasPage
+  page.showAtletaInfo(name)
 }
