edb81b5ce39012203c6456cb57f720f89b2d6eef
zehmurilo <zehm14@gmail.com>
2016-05-21 17:26:57 -0300

    update e adição de metodos

--- test/functional/steps/AtletaTestAndDataOperations.groovy
+++ test/functional/steps/AtletaTestAndDataOperations.groovy
@@ -6,7 +6,7 @@ class AtletaTestAndDataOperations {
 
 
     static atleta = [
-            [name: "Thiago Santos", cpf: "012345678912", data_nascimento: "11/11/2011", contrato:"1"]
+            [name: "Thiago Santos", cpf: "012345678912", data_nascimento: "11/11/2011", contrato:"1", num_jogados:0]
 
     ]
 
@@ -17,10 +17,15 @@ class AtletaTestAndDataOperations {
         }
     }
 
-    public static createAtleta(String nome, String cpf , String data_nascimento, String contrato){
-        def controller = new AtletaController()
-        controller.params << [nome: nome, cpf: cpf, data_nascimento: data_nascimento, contrato: contrato]
+    public static def findByNome(String nome){
+        atleta.find{    atleta ->
+            atleta.nome == nome
+        }
+    }
 
+    public static createAtleta(String nome, String cpf , String data_nascimento, String contrato, int num_jogados){
+        def controller = new AtletaController()
+        controller.params << [nome: nome, cpf: cpf, data_nascimento: data_nascimento, contrato: contrato, num_jogados: num_jogados]
         controller.request.setContent(new byte[1000])
         controller.saveAtleta(controller.criarAtleta())
         controller.response.reset()
@@ -28,11 +33,29 @@ class AtletaTestAndDataOperations {
 
     public static void removeAtleta(def atleta){
         def controller = new AtletaController()
-        controller.params << [id: atleta.getcpf()]
+        controller.params << [id: atleta.getCpf()]
         controller.request.setContent(new byte[1000])
-        controller.delete()
+        controller.search()
         controller.response.reset()
 
     }
 
+    public static void searchAtleta(def atleta) {
+        def controller = new AtletaController()
+        controller.params << [id: atleta.getCpf()]
+        controller.request.setContent(new byte[1000])
+        controller.delete()
+        controller.response.reset()
+    }
+    public static String getInfo(String cpf) {
+        def controller = new AtletaController()
+        String atletaNome = controller.findByCpf(cpf).getNome()
+        String atletaCpf = cpf
+        String atletaDataNascimento = controller.findByCpf(cpf).getdata_nascimento()
+        String atletaContrato = controller.findByCpf(cpf).getContrato()
+        int atletaJogos = controller.findByCpf(cpf).getNum_jogados()
+        atletaInfo = (" Nome: "+atletaNome+"/n CPF: "+atletaCpf+"/n Data de Nascimento: "+atletaDataNascimento+"/n Identificador do Contrato: "+atletaContrato+"/n Número de Jogos: "+atletaJogos)
+        return atletaInfo
+
+    }
 }
