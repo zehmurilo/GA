d48589801f64e70969d3dab068891368de9b40e6
zehmurilo <zehm14@gmail.com>
2016-05-21 17:26:20 -0300

    correção da page

--- test/functional/page/CreatePage.groovy
+++ test/functional/page/CreatePage.groovy
@@ -13,11 +13,12 @@ class CreatePage extends Page {
         title ==~ /Criar Atleta/
     }
 
-    boolean criarAtleta(nome, cpf, data_nascimento, contrato){
+    boolean criarAtleta(nome, cpf, data_nascimento, contrato, num_jogados){
         $("form").nome = nome
         $("fomr").cpf = cpf
         $("form").data_nascimento = data_nascimento
         $("form").contrato = contrato
+        $("form").num_jogados = num_jogados
         $("input", name: "create").click()
     }
 }
