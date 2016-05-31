cbee42dae73e83ba12c3cce4fec44028b988a446
edymiretienne <edymiretienne@gmail.com>
2016-05-31 17:02:18 -0300

    ajustatando as configurações para rodar a aplicação e os testes

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,6 +5,7 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
+grails.server.port.http = 8070
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 
