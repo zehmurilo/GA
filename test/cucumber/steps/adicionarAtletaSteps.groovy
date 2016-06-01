e3205c733c93db0460c634b3c3d479a598b7b7f7
edymiretienne <edymiretienne@gmail.com>
2016-06-01 12:40:02 -0300

    aplicação rodando e teste também (com erros)

---
+++ test/cucumber/steps/adicionarAtletaSteps.groovy
@@ -0,0 +1,76 @@
+package steps
+
+import cucumber.api.PendingException
+import gaatleta.AtletaController
+import gaatleta.Atleta
+
+import pages.AtletasPage
+import pages.CreateAtleta
+this.metaClass.mixin(cucumber.api.groovy.Hooks)
+this.metaClass.mixin(cucumber.api.groovy.EN)
+//Scenario: Adicionar Atleta sem sucesso
+
+Given(~'^Um atleta de CPF "([^"]*)" se encontra cadastrado no sistema$') { String nome, cpf ->
+	def controlador = new AtletaController()
+	createAtleta("Edinaldo", cpf, controlador)
+	assert Atleta.findByCpf(cpf) != null
+}
+
+When(~'^Tento cadastrar um novo atleta "([^"]*)" com o CPF "([^"]*)"$') { String nome, cpf ->
+	def controlador = new AtletaController()
+	verificaAtleta  (nome, cpf, controlador)
+}
+
+Then(~'^O sistema não permite o cadastro duplicado do CPF "([^"]*)"$') { String cpf ->
+	atleta = Atleta.findAllByCpf(cpf)
+	assert atletas.size()==1
+}
+
+def createAtleta(String nome, String cpf, AtletaController controlador) {
+	controlador.params << [cpf: cpf, nome: nome]
+	controlador.save()
+	controlador.response.reset()
+	// eddy tromba
+}
+
+def verificaAtleta(String nome, String cpf, AtletaController controlador) {
+	if (Atleta.findByCpf(cpf)==null) {
+		controlador.params << [cpf: cpf, nome: nome]
+		controlador.save()
+		controlador.response.reset()
+	}
+	else{
+		flash.message = "CPF já cadastrado"
+	}
+}
+
+//web
+Given(~'^Um atleta de CPF "([^"]*)" se encontra cadastrado no sistema$'){ String cpf ->
+	to CreateAtleta
+	at CreateAtleta
+	page.createAtleta("Jankaukas", cpf)
+}
+
+And(~'^Estou no menu Atletas$'){->
+	to AtletasPage
+	at AtletasPage
+}
+
+
+When(~'^Seleciono a opção Adicionar Atleta$'){->
+	to CreateAtleta
+    at CreateAtleta
+}
+
+And(~'^Tento cadastrar um novo atleta "([^"]*)" com o CPF "([^"]*)$'){ String nome, cpf ->
+	to CreateAtleta
+	at CreateAtleta
+	page.verificaAtleta(nome, cpf)
+}
+
+Then(~'Eu posso ver que na página de Atletas o no atleta não foi adicionado$'){->
+	to AtletasPage
+	at AtletasPage
+}
+
+//criar AtletasPage
---
+++ test/functional/page/AtletasPage.groovy
@@ -0,0 +1,11 @@
+package page
+
+import geb.Page
+
+class AtletasPage extends Page {
+	static url = "/GA/atleta/overview/"
+
+	static at = {
+		title ==~ /Lista de Atletas/
+	}
+}
